6fefbd2c9f72eb3b841cf2d2acf57105565fdb1c
eljay16163 <66219175+eljay16163@users.noreply.github.com>
2020-06-05 23:24:06 +0100

    Update Samuel-Ajayi.js

--- scripts/Samuel-Ajayi.js
+++ scripts/Samuel-Ajayi.js
@@ -1,4 +1,3 @@
 function	hng(name, id,language, email) {
-	console.log(`Hello world, this is ${name} with HNGi7 ID ${id} and ${email} using ${language} for stage 2 task`)
-}
-hng ("Ajayi Samuel Toluwa", "HNG-00302", "javascript", "troysammie7@gmail.com")
+	console.log(`Hello world, this is ${name} with HNGi7 ID ${id} ${email} using ${language} for stage 2 task`)
+} hng ("Ajayi Samuel Toluwa", "HNG-00302", "javascript", "troysammie7@gmail.com")
